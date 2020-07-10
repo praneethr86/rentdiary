183869fa6a3633a4dc481c2ffab9142d8c01a03c
Praneeth Patakota <praneeth001@gmail.com>
2020-07-10 15:26:04 +0530

    Add tenant details screen

--- lib/screens/tenantdetails/components/body.dart
+++ lib/screens/tenantdetails/components/body.dart
@@ -1,23 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:rentdiary/constants.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
+    return Padding(
+      padding: EdgeInsets.only(
+          top: kDefaultPadding * 2, left: kDefaultPadding * 1.5),
+      child: Container(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                RaisedButton(
-                  child: Text('Back'),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                ),
-              ],
-            )
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Name'),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Address'),
+            ),
+            ListTile(
+              leading: Icon(Icons.call),
+              title: Text('Contact No'),
+            ),
+            ListTile(
+              leading: Icon(Icons.work),
+              title: Text('Occupation'),
+            ),
+            ListTile(
+              leading: Icon(Icons.date_range),
+              title: Text('Occupied Date'),
+            ),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text('Vacated Date'),
+            ),
+            ListTile(
+              leading: Icon(Icons.payment),
+              title: Text('Advance Paid'),
+            ),
+            ListTile(
+              leading: Icon(Icons.people),
+              title: Text('Family Members'),
+            ),
           ],
         ),
       ),
